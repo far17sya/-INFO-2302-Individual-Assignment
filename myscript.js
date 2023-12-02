a8f3914569613f0af2f1e94c42a87bceb35d7e32
far17sya <132445811+far17sya@users.noreply.github.com>
2023-12-02 13:28:09 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+document.getElementById("b").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+    console.log("The form has been submitted! ");
+});
+
+document.getElementById("c").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+    console.log("The form has been submitted! ");
+});
